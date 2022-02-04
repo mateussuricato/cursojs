c8fc30a59100a077a0415e78665ec83a8b60b6c7
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-02-04 11:49:25 -0300

    js

---
+++ aulas/aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+
+var c = 1
+while (c <= 1000) {
+    console.log(c)
+    c++
+}
+
+*/
+
+console.log('Vai começar...')
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('FIM')
